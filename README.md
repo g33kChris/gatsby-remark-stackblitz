5ce2dfc3355e124b789f85d9ee5f1e46b86d7669
Chris Wallace <chris@g33kchris.net>
2019-10-03 13:00:35 +0100

    Initial working embed iframe based off of the codepen embed solution.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gatsby-remark-stackblitz
-Embed stackblitz apps in Gatsby Markdown.
+Based upon the excellent plugin ()[https://github.com/weknowinc/gatsby-remark-codepen].
+Adds support to embed stackblitz apps using Gatsby Markdown.
--- index.js
+++ index.js
@@ -1,4 +1,42 @@
+const visit = require('unist-util-visit');
+
 module.exports = async({ markdownAST }, options = { width: 600, height: 300}) => {
+
+    visit(markdownAST, 'text', async (node) => {
+        const { value } = node;
+        const isStackblitzURI = value.match(/https:\/\/(www\.)?stackblitz\.com\/edit\/([A-Za-z0-9-_?=]*)/gi);
+    
+        //https://stackblitz.com/edit/angular?embed=1
+
+        if (isStackblitzURI) {
+        //   const penData = value.split('/');
+        //   const penId = penData.pop();
+        //   const penUser = penData[3];
+          console.log(`\n Embeding stackblitz app: APPID by APP published \n`);
+    
+        // <iframe src="https://stackblitz.com/edit/angular?embed=1"></iframe>
+
+        node.type = 'html';
+        node.value = `<div><iframe 
+          height='${options.height}' 
+          scrolling='no' 
+          src='//stackblitz.com/edit/angular?embed=1' 
+          frameborder='no' 
+          allowtransparency='true' 
+          allowfullscreen='true' 
+          style='width: 100%;'></iframe></div>`;
+
+        //   node.type = 'html';
+        //   node.value = `<div><iframe 
+        //     height='${options.height}' 
+        //     scrolling='no' 
+        //     src='//codepen.io/${penUser}/embed/preview/${penId}/?height=${options.height}&theme-id=${options.theme}&default-tab=${options.defaultTab || 'html,result'}' 
+        //     frameborder='no' 
+        //     allowtransparency='true' 
+        //     allowfullscreen='true' 
+        //     style='width: 100%;'></iframe></div>`;
+        }
+    });
 
     return markdownAST;
 };
